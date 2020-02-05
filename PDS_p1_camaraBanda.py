deb32b3b7430e4d9b84b76f6904b1f938967cd3d
Victor Flores <vflorespineda@gmail.com>
2020-02-05 17:55:33 -0600

    practica 1 terminada

--- PDS_p1_camaraBanda.py
+++ PDS_p1_camaraBanda.py
@@ -1,53 +1,89 @@
 #Víctor Hugo Flores Pineda 155990
 #Rebeca Baños García 157655
 #Python 3
-
 import numpy as np 
 import matplotlib.pyplot as plt
 
-t = np.linspace(0,1000,1000) / 1000
+ts = np.linspace(0,1000,1000) / 1000
 fs = 4
-y = np.cos(2*np.pi*fs*t)
+ys = np.cos(2*np.pi*fs*ts)
 
 plt.subplot(611)
 plt.title("Señal original")
-plt.plot(t,y)
+plt.plot(ts,ys)
 
+#Ejercicio 1
 
 plt.subplot(612)
-
-t = np.linspace(0,1000,1000) / 1000
-y = np.cos(2*np.pi*fs*t)
-plt.plot(t,y)
 plt.title("16 muestras por periodo")
-t = np.linspace(1,1000,48) / 1000
+plt.plot(ts,ys)
+t = np.linspace(0,1000,65) / 1000
 y = np.cos(2*np.pi*fs*t)
 plt.stem(t,y)
 
-t = np.linspace(1,1000,24) / 1000
-y = np.cos(2*np.pi*fs*t)
 plt.subplot(613)
 plt.title("8 muestras por periodo")
+plt.plot(ts,ys)
+t = np.linspace(0,1000,33) / 1000
+y = np.cos(2*np.pi*fs*t)
 plt.stem(t,y)
 
-t = np.linspace(1,1000,12) / 1000
-y = np.cos(2*np.pi*fs*t)
 plt.subplot(614)
 plt.title("4 muestras por periodo")
+plt.plot(ts,ys)
+t = np.linspace(0,1000,17) / 1000
+y = np.cos(2*np.pi*fs*t)
 plt.stem(t,y)
 
-t = np.linspace(1,1000,6) / 1000
-y = np.cos(2*np.pi*fs*t)
 plt.subplot(615)
 plt.title("2 muestras por periodo")
+plt.plot(ts,ys)
+t = np.linspace(0,1000,9) / 1000
+y = np.cos(2*np.pi*fs*t)
 plt.stem(t,y)
 
-t = np.linspace(1,1000,3) / 1000
-y = np.cos(2*np.pi*fs*t)
 plt.subplot(616)
 plt.title("1 muestras por periodo")
+plt.plot(ts,ys)
+t = np.linspace(0,1000,5) / 1000
+y = np.cos(2*np.pi*fs*t)
 plt.stem(t,y)
 
+
+#Ejercicio 2
+plt.figure()
+
+plt.subplot(611)
+plt.title("Señal original analógica")
+plt.plot(ts,ys)
+
+plt.subplot(612)
+plt.title("Señal discreta")
+t = np.linspace(0,1000,150) / 1000
+y = 5*np.cos(2*np.pi*fs*t)
+plt.stem(t,y)
+
+plt.subplot(613)
+plt.title("Señal discreta y cuantificada")
+yRound = np.round(5*np.cos(2*np.pi*fs*t))
+plt.stem(t,yRound)
+
+plt.subplot(614)
+plt.title("Error de cuantificación usando round")
+yErrR = yRound - y
+plt.stem(t,yErrR)
+
+plt.subplot(615)
+plt.title("Error de cuantificación usando floor")
+yFloor = np.floor(5*np.cos(2*np.pi*fs*t))
+yErrR = yFloor - y
+plt.stem(t,yErrR)
+
+plt.subplot(616)
+plt.title("Error de cuantificación usando ceil")
+yCeil = np.ceil(5*np.cos(2*np.pi*fs*t))
+yErrR = yCeil - y
+plt.stem(t,yErrR)
 
 
 plt.show()
