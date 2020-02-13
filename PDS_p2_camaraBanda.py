61a08932ae91fffb91a33457b9895a16fc373b27
Victor Flores <vflorespineda@gmail.com>
2020-02-13 17:20:28 -0600

    practica 2 terminada

--- PDS_p2_camaraBanda.py
+++ PDS_p2_camaraBanda.py
@@ -7,12 +7,15 @@ import huffman as hf
 
 #1
 x = np.random.randint(1,6,20)
+print("Cadena aleatoria de numeros: ")
 print(x)
+print("\n")
 
 #2
 xB = np.zeros(20)
 for i in range(0,20):
     xB[i] = np.binary_repr(x[i],3)
+print("Representación binaria de la cadena original:")
 print(xB)
 
 longT = 0
@@ -21,6 +24,7 @@ for i in range(0,20):
 print("Longitud total de cadena bianria: " + str(longT))
 longP = longT/20
 print("Longitud pormedio por simbolo: " + str(longP))
+print("\n")
 
 #4
 #Seis bits
@@ -44,13 +48,53 @@ def prob(vec):
     return p
 
 probabilidad = prob(x)
+print("probabilidad de aparición de cada simbolo: ")
 print(probabilidad)
+print("\n")
 
 #5 
 huffVec = ([('1',probabilidad[0]*20), ('2',probabilidad[1]*20), ('3',probabilidad[2]*20), ('4',probabilidad[3]*20),('5',probabilidad[4]*20)])
 xH = hf.codebook(huffVec)
+print("Codigo Huffman: ")
 print(xH)
 longT = len(xH['1']) + len(xH['2']) + len(xH['3']) + len(xH['4']) + len(xH['5'])
-print("Longitud total de cadena Huffman: " + str(longT))
 longP = longT/5
 print("Longitud pormedio por simbolo: " + str(longP))
+print("\n")
+
+#6
+cadH = ["" for x in range(len(x))]
+longT = 0
+for i in range(0,len(x)):
+    if(x[i] == 1):
+        cadH[i] = xH['1']
+    elif (x[i] == 2):
+        cadH[i] = xH['2']
+    elif (x[i] == 3):
+        cadH[i] = xH['3']
+    elif (x[i] == 4):
+        cadH[i] = xH['4']
+    elif (x[i] == 5):
+        cadH[i] = xH['5']
+    longT = longT + len(cadH[i])  
+
+print("Cadena aleatoria original en representación Huffman:")
+print(cadH)
+print("Longitud total de cadena Huffman: " + str(longT))
+
+#8
+cadDec = np.zeros(len(cadH))
+for i in range(0,len(cadH)):
+    if(cadH[i] == xH['1']):
+        cadDec[i] = 1
+    elif (cadH[i] == xH['2']):
+        cadDec[i] = 2
+    elif (cadH[i] == xH['3']):
+        cadDec[i] = 3
+    elif (cadH[i] == xH['4']):
+        cadDec[i] = 4
+    elif (cadH[i] == xH['5']):
+        cadDec[i] = 5
+
+print("Cadena decodificada")
+print(cadDec)
