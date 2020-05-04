3c5a70d033b8e31651a2d5b66747106f06491224
Victor Flores <vflorespineda@gmail.com>
2020-05-04 13:53:01 -0500

    transientes

--- PDS_p8_camaraBanda.py
+++ PDS_p8_camaraBanda.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
 from scipy.io import wavfile as wv
-import librosa.core
+import librosa.core as lc
 
 
 fs, x = wv.read('./audios/camaraA.wav')
@@ -13,15 +13,18 @@ n = len(x)
 ham = scipy.signal.hamming(n)
 mult = ham*x[:,1]
 y = scipy.signal.lfilter([1],[1,0.63],mult)
-a = librosa.core.lpc(y,10)
-h, w = scipy.signal.freqz([1],a,n)
+a = lc.lpc(y,10)
+y_hat = scipy.signal.lfilter([0] + -1*a[1:], [1], y)
+plt.plot(y)
+plt.plot(y_hat,'g--')
 
-raices = np.roots(a)
-
-
-
-Ftransform = np.fft.fft(x)
+w, h = scipy.signal.freqz([0] + -1*a[1:], [1],n)
+FFT = scipy.fftpack.fft(x)
 plt.figure()
-plt.plot(Ftransform)
+plt.plot(w,h,'C1')
+plt.plot(w,FFT,'g')
+
+
+
 
 plt.show()
